9b5b355294a4bcd935e817a28d3ec17289d9b8cd
Iuliia Morozova <yulii.morozova@gmail.com>
2026-02-25 13:02:49 +0100

    Initial Commit

---
+++ DailyChallange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyChallange
+//
+//  Created by Julia Morozova on 25. 2. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyChallange/DailyChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyChallangeApp.swift
+//  DailyChallange
+//
+//  Created by Julia Morozova on 25. 2. 2026..
+//
+
+import SwiftUI
+
+@main
+struct DailyChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
